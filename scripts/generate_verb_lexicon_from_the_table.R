2b8a625c817ed987ccc120c9af3432046e541c72
George Moroz <agricolamz@gmail.com>
2023-05-16 20:36:55 +0300

    add verbs; fix twol; add personal pronouns

--- scripts/generate_verb_lexicon_from_the_table.R
+++ scripts/generate_verb_lexicon_from_the_table.R
@@ -12,7 +12,7 @@ read_csv("dicts/verb_dictionary.csv", show_col_types = FALSE) |>
   arrange(-n, tag, lemma) |> 
   mutate(translation_en = ifelse(is.na(translation_en), "", translation_en),
          tag = ifelse(is.na(tag), "", tag),
-         generate = str_c(lemma, ":", form, "[", tag, "]"),
+         generate = str_c(lemma, "<V>:", form, "[", tag, "]"),
          generate = str_pad(generate, width = 60, side = "right"),
          generate = str_c(generate, "# ", translation_ru, "; ", translation_en)) |> 
   pull(generate) |> 
